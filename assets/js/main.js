119451b54d3ca89ace21c5e048acdfb15e605aa7
jorgeart81 <aeroestadistica@gmail.com>
2021-10-12 11:54:11 -0500

    Se agrega el Proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/* NAV */
+// Función invocada inmendiatamente IIFE
+(function () {
+    let navbar = document.querySelector('#navbar');
+    let hamburger = document.querySelector('#hamburger');
+    hamburger.addEventListener('click', () => {
+        navbar.classList.toggle('open');
+    });
+})()
